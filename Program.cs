557747a6932d03666c1558590a2f8e0529d32bcb
Jon Mindingall <jminding@yahoo.com>
2020-10-08 20:11:12 -0500

    New program

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    
+    class CalculateArea
+    {
+        public double areaOfCircle(double r)
+        {
+            double area = Math.PI * (r * r);
+            return area;
+
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int a;
+            int b;
+            int quotient;
+            
+            a = 17;
+            b = 4;
+            quotient = a / b;
+            int remainder = a % b;
+            CalculateArea s = new CalculateArea();
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+            Console.WriteLine("What is the radius of your circle?");
+            double radius = double.Parse(Console.ReadLine());
+            double area = s.areaOfCircle(radius);
+            Console.WriteLine($"The area of a circle with radius of {radius} is {area}");
+            
+
+        }
+        
+    }
+}
